54fbd7247ecb677302563a8a63160491381475a1
KartikeyKakaria <81517323+KartikeyKakaria@users.noreply.github.com>
2023-06-12 09:30:38 +0530

    added earthCanvas and contacts ui

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,14 +1,12 @@
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
-
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-x={0} rotation-y={0} />
   );
 };
 
@@ -16,11 +14,10 @@ const EarthCanvas = () => {
   return (
     <Canvas
       shadows
-      frameloop='demand'
-      dpr={[1, 2]}
+      frameloop="demand"
       gl={{ preserveDrawingBuffer: true }}
       camera={{
-        fov: 45,
+        fov: 52,
         near: 0.1,
         far: 200,
         position: [-4, 3, 6],
@@ -34,8 +31,6 @@ const EarthCanvas = () => {
           minPolarAngle={Math.PI / 2}
         />
         <Earth />
-
-        <Preload all />
       </Suspense>
     </Canvas>
   );
